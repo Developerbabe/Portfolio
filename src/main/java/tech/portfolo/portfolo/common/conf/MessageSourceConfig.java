6cac6f4f1e3848ad7c805e21dc936e2753c3c9f8
Developerbabe <82949317+Developerbabe@users.noreply.github.com>
2022-10-28 12:27:10 +0900

    MessageSourceAccessor Add Config

---
+++ src/main/java/tech/portfolo/portfolo/common/conf/MessageSourceConfig.java
@@ -0,0 +1,30 @@
+package tech.portfolo.portfolo.common.conf;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class MessageSourceConfig {
+
+    @Bean
+    public MessageSource messageSource(){
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        String[] basenames = new String[] {
+                "classpath:i18n/message",
+                "classpath:i18n/error"
+        };
+        messageSource.setBasenames(basenames);
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setCacheSeconds(60);
+
+        return messageSource;
+    }
+
+    @Bean
+    public MessageSourceAccessor messageSourceAccessor(){
+        return new MessageSourceAccessor(messageSource());
+    }
+}
